c78c3daa4f165bbf9015869655d525f23568f1b0
snoopycheng <yichiacheng2001@gmail.com>
2024-12-27 00:29:30 +0800

    1227

--- 169_majority_element.cpp
+++ 169_majority_element.cpp
@@ -4,3 +4,24 @@
 #include<vector>
 using namespace std;
 
+class Solution{
+public:
+    int majorityElement(vector<int>& nums){
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
