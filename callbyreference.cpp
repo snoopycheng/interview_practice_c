f31a10e0c63d55108a5751e58c2d540e9245d703
Yi-Chia Cheng <128214321+snoopycheng@users.noreply.github.com>
2025-03-07 14:29:51 +0800

    Update callbyreference.cpp

--- callbyreference.cpp
+++ callbyreference.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// 式接收的是變數的引用（C++ 提供 &），修改時直接作用於原變數
+// 函式接收的是變數的引用（C++ 提供 &），修改時直接作用於原變數
 // C 語言不直接支援 Call by Reference，但可以通過指標模擬（即 Call by Address）
 // 適用於 C++，可用於物件傳遞時避免大量複製開銷
 
